948b8c536dea3f5185a9c5b92802bcd22ae1e447
BijonDurjoy <bijon176193@gmail.com>
2023-09-13 10:13:10 +0600

    This is a test

--- a1.cpp
+++ a1.cpp
@@ -6,23 +6,7 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    llu n,i,ans=1;
-    cin >> n;
-    llu a[n];
-    for(i=0;i<n;i++){
-        cin >> a[i];
-        if(a[i]==0){
-            cout << "0\n";
-            return 0;
-        }
-    }
-    for(i=0;i<n;i++){
-        if(a[i]>1000000000000000000/ans){
-            cout << "-1\n";
-            return 0;
-        }
-        ans*=a[i];
-    }
-    cout << ans << "\n";
+    
+    cout<<"This is a test!"<<endl;
     return 0;
 }
